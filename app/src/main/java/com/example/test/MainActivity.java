ec01fee180389535c3366a96a7fb5b51f830d9f2
Muskan <muskanbatham161@gmail.com>
2023-12-05 11:37:54 +0530

    Initial Commit

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+    }
+    public void PregnancyDiet(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void PCOSDiet(View view) {
+        Intent intentt = new Intent(this, MainActivity3.class);
+        startActivity(intentt);
     }
 }
